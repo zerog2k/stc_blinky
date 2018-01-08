2bf940bf0c005e1c020b265d6312bff5507505ef
Jens Jensen <af5mi@yahoo.com>
2018-01-07 19:05:34 -0600

    sync makefile and pio

--- src/serial.h
+++ src/serial.h
@@ -8,6 +8,8 @@
 
 void UART_INIT(void);
 void _tm1() __interrupt 3 __using 1;
-//void putchar(unsigned char);
+#ifndef PLATFORMIO
+void putchar(unsigned char);
+#endif
 #define uart_put_char putchar
 #define _putchar putchar
