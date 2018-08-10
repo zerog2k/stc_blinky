2bb9f26a656aa1772961ece6be4820723ee5121e
Jens J <af5mi@yahoo.com>
2018-08-10 12:57:39 -0500

    changes to support latest platformio

--- extra_script.py
+++ extra_script.py
@@ -7,25 +7,7 @@ env = DefaultEnvironment()
 
 print "custom extra_script launching..."
 
-# env.Replace(
-#     MYUPLOADERFLAGS=[
-#         "-v",
-#         "-p", "$BOARD_MCU",
-#         "-C",
-#         '"%s"' % join("$PIOPACKAGES_DIR", "tool-avrdude", "avrdude.conf"),
-#         "-c", "$UPLOAD_PROTOCOL",
-#         "-b", "$UPLOAD_SPEED"
-#      ],
-#     UPLOADHEXCMD='"$UPLOADER" $MYUPLOADERFLAGS -U flash:w:$SOURCES:i'
-# )
-
-if "BOARD" in env and env.BoardConfig().get("vendor") == "STC":
-    f_cpu_khz = int(env.BoardConfig().get("build.f_cpu")) / 1000
-    env.Replace(
-            UPLOAD_OPTIONS = [
-                "-p", "$UPLOAD_PORT",
-                "-P", "stc15a",
-                "-t", int(f_cpu_khz),
-            ],
-            UPLOADHEXCMD = "stcgal/stcgal.py $UPLOAD_OPTIONS $SOURCE"
-    )
+env.Append(
+    # stcgal expected in stcgal subdir
+    STCGALCMD="/stcgal.py"
+)
